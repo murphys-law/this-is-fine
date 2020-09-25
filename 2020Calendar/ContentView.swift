f0372bcf4b6250a4da1d5cc41ae0cdd45b3559b1
Claire Murphy <cmurphy@squareup.com>
2020-09-25 15:01:52 -0700

    Blank project

---
+++ 2020Calendar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  2020Calendar
+//
+//  Created by Claire Murphy on 9/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Today is")
+            Text("March 209th, 2020").font(Font()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
